24368be4622be60a59ea5ae2f6883271244bcfce
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-09-04 00:13:36 -0300

    feat: add simples http server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+
+	// In this simple logic we have: TCP Connection -> HTTP Server -> Parse HTTP -> HTTP Request -> Handler -> Response Writer -> HTTP Response
+	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World!")
+	})
+
+	log.Println("Server listening on :8080") // Default PORT
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
