7250108b3ad71550ddeb24d1349beab17632a41d
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-09-05 23:43:23 -0300

    feat: add handler concept with inspector handler getting all localhost:8080 requests

--- main.go
+++ main.go
@@ -1,75 +1,28 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-// Exercise: Create a HTTP inspector. `GET /inspect` should return:
-// {
-//   "method": "GET",
-//   "path": "/inspect",
-//   "protocol": "HTTP/1.1",
-//   "host": "localhost:8080",
-//   "userAgent": "curl/..."
-// }
-// And add a `POST /echo` to the curl responde a "hello rift".
+type InspectorHandler struct{}
 
-type InspectResponse struct {
-	Method    string `json:"method"`
-	Path      string `json:"path"`
-	Protocol  string `json:"protocol"`
-	Host      string `json:"host"`
-	UserAgent string `json:"user_agent"`
-}
-
-func inspectHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	data := InspectResponse{
-		Method:    r.Method,
-		Path:      r.URL.Path,
-		Protocol:  r.Proto,
-		Host:      r.Host,
-		UserAgent: r.UserAgent(),
-	}
-
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Printf("failed to encode response: %v", err)
-	}
-}
-
-func ecoHandler(w http.ResponseWriter, r *http.Request) {
+func (h InspectorHandler) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
 	w.Header().Set("Content-Type", "text/plain")
 
-	// The Copy concept comes from Streaming.
-	// If a 5GB Request is sent, the proxy doesn't gonna read everything and send to memory.
-	// Instead, he'll work over parts of the data withou maintaning the entire content.
-
-	// Caso uma requisição de 5GB seja enviada, o proxy não vai ler tudo e enviar para a memória.
-	// Ele vai trabalhar sobre partes dos dados sem precisar manter o conteúdo inteiro.
-
-	// To Test:
-	// curl -i \
-	// 	-X POST \
-	// 	http://localhost:8080/echo \
-	// 	-d 'hello rift'
-	if _, err := io.Copy(w, r.Body); err != nil {
-		http.Error(w, "failed to copy request body", http.StatusInternalServerError)
-	}
+	fmt.Fprintf(w, "%s %s", r.Method, r.URL.Path)
 }
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /inspect", inspectHandler)
-	mux.HandleFunc("POST /echo", ecoHandler)
+	handler := InspectorHandler{}
 
 	log.Println("server listening on 8080")
 
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
 }
