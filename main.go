6ce61b83d3b066fbd6adaabc8b3992bfcbe0ed36
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-09-05 21:31:57 -0300

    feat: add example for handler a HTTP request

--- main.go
+++ main.go
@@ -6,30 +6,18 @@ import (
 	"net/http"
 )
 
+type RiftHandler struct{}
+
+func (h *RiftHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Rift intercepted %s %s\n", r.Method, r.URL.Path)
+}
+
 func main() {
-	mux := http.NewServeMux()
+	handler := RiftHandler{}
 
-	// In this simple logic we have: TCP Connection -> HTTP Server -> Parse HTTP -> HTTP Request -> Handler -> Response Writer -> HTTP Response
-	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
-	})
+	log.Println("Server listening on 8080")
 
-	// Execute and see the output:
-	// curl -v \
-	// 	-H 'X-Rift-Test: hello' \
-	// 	http://localhost:8080/debug
-
-	mux.HandleFunc("GET /debug", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Method: %s\n", r.Method)
-		fmt.Fprintf(w, "Path: %s\n", r.URL.Path)
-		fmt.Fprintf(w, "Host: %s\n", r.Host)
-		fmt.Fprintf(w, "Protocol: %s\n", r.Proto)
-		fmt.Fprintf(w, "Header: %s\n", r.Header.Get("X-Rift-Test"))
-	})
-
-	log.Println("Server listening on :8080") // Default PORT
-
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", &handler); err != nil {
 		log.Fatal(err)
 	}
 }
