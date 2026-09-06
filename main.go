e2291b689324a017d3e7eb2aa5806e96ece6f5d9
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-09-05 21:09:42 -0300

    feat: add debug endpoint to HTTP server

--- main.go
+++ main.go
@@ -14,6 +14,19 @@ func main() {
 		fmt.Fprintln(w, "Hello, World!")
 	})
 
+	// Execute and see the output:
+	// curl -v \
+	// 	-H 'X-Rift-Test: hello' \
+	// 	http://localhost:8080/debug
+
+	mux.HandleFunc("GET /debug", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Method: %s\n", r.Method)
+		fmt.Fprintf(w, "Path: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Host: %s\n", r.Host)
+		fmt.Fprintf(w, "Protocol: %s\n", r.Proto)
+		fmt.Fprintf(w, "Header: %s\n", r.Header.Get("X-Rift-Test"))
+	})
+
 	log.Println("Server listening on :8080") // Default PORT
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
